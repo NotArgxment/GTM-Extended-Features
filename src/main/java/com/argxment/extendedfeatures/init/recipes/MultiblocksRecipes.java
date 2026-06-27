3f100915ebdab950e70bdd82e9e4a54fab4db8f3
Usuario <therealxkorpion@gmail.com>
2026-06-27 17:01:22 -0300

    what

--- src/main/java/com/argxment/extendedfeatures/init/recipes/MultiblocksRecipes.java
+++ src/main/java/com/argxment/extendedfeatures/init/recipes/MultiblocksRecipes.java
@@ -12,91 +12,67 @@ import java.util.function.Consumer;
 
 import static com.argxment.extendedfeatures.client.RecipeTypes.ROCK_PROCESSING;
 
+import static com.gregtechceu.gtceu.api.GTValues.L;
 import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.dust;
 import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 
 public class MultiblocksRecipes {
 
-    /*
+    /**
      * Actual controller recipes are supposed to be made by modpack developers
      * This file will contain only recipes for the custom recipes types
-    */
+     */
 
     public static void init(Consumer<FinishedRecipe> provider) {
 
         ROCK_PROCESSING.recipeBuilder("deepslate_processing")
-                .inputItems(new ItemStack(Blocks.DEEPSLATE), 256)
-                .perTick(true)
-                    .inputFluids(DrillingFluid.getFluid(5))
-                .perTick(false)
-                .outputItemsRanged(dust, Potassium, UniformInt.of(1,128))
-                .outputItemsRanged(dust, Magnesium, UniformInt.of(1,128))
-                .outputItemsRanged(dust, Aluminium, UniformInt.of(1,128))
-                .outputItemsRanged(dust, Silicon, UniformInt.of(1,128))
-                .outputFluidsRanged(
-                        new FluidStack(Fluorine.getFluid(), 1000),
-                            UniformInt.of(1000, 16000)
-                )
-                .outputFluidsRanged(
-                        new FluidStack(Oxygen.getFluid(), 1000),
-                            UniformInt.of(1000, 16000)
-                )
-                .duration(5000)
-                .EUt(GTValues.VA[GTValues.LuV])
+                .inputItems(new ItemStack(Blocks.DEEPSLATE), 1)
+                .inputFluids(Lubricant.getFluid(L * 4))
+                .outputItems(dust, Potassium, 1)
+                .outputItems(dust, Magnesium, 1)
+                .outputItems(dust, Aluminium, 1)
+                .outputItems(dust, Silicon, 1)
+                .outputFluids(Fluorine.getFluid(L * 2))
+                .outputFluids(Oxygen.getFluid(L * 4))
+                .duration(2500)
+                .EUt(GTValues.VA[GTValues.EV])
                 .save(provider);
 
         ROCK_PROCESSING.recipeBuilder("andesite_processing")
-                .inputItems(new ItemStack(Blocks.ANDESITE), 256)
-                .perTick(true)
-                    .inputFluids(DrillingFluid.getFluid(5))
-                .perTick(false)
-                .outputItemsRanged(dust, Magnesium, UniformInt.of(1,128))
-                .outputItemsRanged(dust, Silicon, UniformInt.of(1,128))
-                .outputFluidsRanged(
-                        new FluidStack(Hydrogen.getFluid(), 1000),
-                        UniformInt.of(1000, 16000)
-                )
-                .outputFluidsRanged(
-                        new FluidStack(Oxygen.getFluid(), 1000),
-                        UniformInt.of(1000, 16000)
-                )
-                .duration(5000)
-                .EUt(GTValues.VA[GTValues.LuV])
+                .inputItems(new ItemStack(Blocks.ANDESITE), 1)
+                .inputFluids(Lubricant.getFluid(L * 4))
+                .outputItems(dust, Magnesium, 1)
+                .outputItems(dust, Silicon, 1)
+                .outputFluids(Hydrogen.getFluid(L * 2))
+                .outputFluids(Oxygen.getFluid(L * 4))
+                .duration(2500)
+                .EUt(GTValues.VA[GTValues.EV])
                 .save(provider);
 
         ROCK_PROCESSING.recipeBuilder("diorite_processing")
-                .inputItems(new ItemStack(Blocks.DIORITE), 256)
-                .perTick(true)
-                    .inputFluids(DrillingFluid.getFluid(5))
-                .perTick(false)
-                .outputItemsRanged(dust, Sodium, UniformInt.of(1,96))
-                .outputItemsRanged(dust, Sulfur, UniformInt.of(1,64))
-                .outputFluidsRanged(
-                        new FluidStack(Water.getFluid(), 1000),
-                        UniformInt.of(1000, 64000)
-                )
-                .outputFluidsRanged(
-                        new FluidStack(Oxygen.getFluid(), 1000),
-                        UniformInt.of(1000, 16000)
-                )
-                .duration(5000)
-                .EUt(GTValues.VA[GTValues.LuV])
+                .inputItems(new ItemStack(Blocks.DIORITE), 1)
+                .inputFluids(Lubricant.getFluid(L * 4))
+                .outputItems(dust, Sodium, 1)
+                .outputItems(dust, Sulfur, 1)
+                .outputFluids(Water.getFluid(L * 4))
+                .outputFluids(Oxygen.getFluid(L * 2))
+                .duration(2500)
+                .EUt(GTValues.VA[GTValues.EV])
                 .save(provider);
 
         ROCK_PROCESSING.recipeBuilder("obsidian_processing")
-                .inputItems(new ItemStack(Blocks.OBSIDIAN), 256)
-                .perTick(true)
-                    .inputFluids(DrillingFluid.getFluid(5))
-                .perTick(false)
-                .outputItemsRanged(dust, Magnesium, UniformInt.of(1,96))
-                .outputItemsRanged(dust, Iron, UniformInt.of(1,96))
-                .outputItemsRanged(dust, Silicon, UniformInt.of(1,64))
-                .outputFluidsRanged(
-                        new FluidStack(Oxygen.getFluid(), 1000),
-                        UniformInt.of(1000, 16000)
-                )
-                .duration(5000)
-                .EUt(GTValues.VA[GTValues.LuV])
+                .inputItems(new ItemStack(Blocks.OBSIDIAN), 1)
+                .inputFluids(Lubricant.getFluid(L * 4))
+                .outputItems(dust, Magnesium, 1)
+                .outputItems(dust, Iron, 1)
+                .outputItems(dust, Silicon, 1)
+                .outputFluids(Oxygen.getFluid(L * 3))
+                .duration(2500)
+                .EUt(GTValues.VA[GTValues.EV])
                 .save(provider);
+    }
+
+    private static int getAmount() {
+        return 1000;
     }
 }
