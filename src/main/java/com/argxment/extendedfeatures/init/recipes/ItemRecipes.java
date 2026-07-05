08eaf4fd568dc39708caa2cf4c8ad94e9bddd05c
Usuario <therealxkorpion@gmail.com>
2026-07-05 16:25:11 -0300

    changes all outputs to match the new map for them

--- src/main/java/com/argxment/extendedfeatures/init/recipes/ItemRecipes.java
+++ src/main/java/com/argxment/extendedfeatures/init/recipes/ItemRecipes.java
@@ -1,5 +1,7 @@
 package com.argxment.extendedfeatures.init.recipes;
 
+import com.argxment.extendedfeatures.init.Items;
+
 import com.gregtechceu.gtceu.api.GTCEuAPI;
 import com.gregtechceu.gtceu.api.GTValues;
 
@@ -8,7 +10,6 @@ import net.minecraft.data.recipes.FinishedRecipe;
 
 import java.util.function.Consumer;
 
-import static com.argxment.extendedfeatures.init.Items.*;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLER_RECIPES;
 
 public class ItemRecipes {
@@ -17,7 +18,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("ulv_universal_circuit")
                 .inputItems(CustomTags.ULV_CIRCUITS)
-                .outputItems(ULV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.ULV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -25,7 +26,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("lv_universal_circuit")
                 .inputItems(CustomTags.LV_CIRCUITS)
-                .outputItems(LV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.LV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -33,7 +34,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("mv_universal_circuit")
                 .inputItems(CustomTags.MV_CIRCUITS)
-                .outputItems(MV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.MV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -41,7 +42,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("hv_universal_circuit")
                 .inputItems(CustomTags.HV_CIRCUITS)
-                .outputItems(HV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.HV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -49,7 +50,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("ev_universal_circuit")
                 .inputItems(CustomTags.EV_CIRCUITS)
-                .outputItems(EV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.EV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -57,7 +58,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("iv_universal_circuit")
                 .inputItems(CustomTags.IV_CIRCUITS)
-                .outputItems(IV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.IV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -65,7 +66,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("luv_universal_circuit")
                 .inputItems(CustomTags.LuV_CIRCUITS)
-                .outputItems(LuV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.LuV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -73,7 +74,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("zpm_universal_circuit")
                 .inputItems(CustomTags.ZPM_CIRCUITS)
-                .outputItems(ZPM_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.ZPM])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -81,7 +82,7 @@ public class ItemRecipes {
 
         ASSEMBLER_RECIPES.recipeBuilder("uv_universal_circuit")
                 .inputItems(CustomTags.UV_CIRCUITS)
-                .outputItems(UV_UNIVERSAL_CIRCUIT)
+                .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.UV])
                 .duration(20)
                 .EUt(GTValues.VA[GTValues.LV])
                 .circuitMeta(10)
@@ -90,7 +91,7 @@ public class ItemRecipes {
         if (GTCEuAPI.isHighTier()) {
             ASSEMBLER_RECIPES.recipeBuilder("uhv_universal_circuit")
                     .inputItems(CustomTags.UHV_CIRCUITS)
-                    .outputItems(UHV_UNIVERSAL_CIRCUIT)
+                    .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.UHV])
                     .duration(20)
                     .EUt(GTValues.VA[GTValues.LV])
                     .circuitMeta(10)
@@ -98,7 +99,7 @@ public class ItemRecipes {
 
             ASSEMBLER_RECIPES.recipeBuilder("uev_universal_circuit")
                     .inputItems(CustomTags.UEV_CIRCUITS)
-                    .outputItems(UEV_UNIVERSAL_CIRCUIT)
+                    .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.UEV])
                     .duration(20)
                     .EUt(GTValues.VA[GTValues.LV])
                     .circuitMeta(10)
@@ -106,7 +107,7 @@ public class ItemRecipes {
 
             ASSEMBLER_RECIPES.recipeBuilder("uiv_universal_circuit")
                     .inputItems(CustomTags.UIV_CIRCUITS)
-                    .outputItems(UIV_UNIVERSAL_CIRCUIT)
+                    .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.UIV])
                     .duration(20)
                     .EUt(GTValues.VA[GTValues.LV])
                     .circuitMeta(10)
@@ -114,7 +115,7 @@ public class ItemRecipes {
 
             ASSEMBLER_RECIPES.recipeBuilder("uxv_universal_circuit")
                     .inputItems(CustomTags.UXV_CIRCUITS)
-                    .outputItems(UXV_UNIVERSAL_CIRCUIT)
+                    .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.UXV])
                     .duration(20)
                     .EUt(GTValues.VA[GTValues.LV])
                     .circuitMeta(10)
@@ -122,12 +123,11 @@ public class ItemRecipes {
 
             ASSEMBLER_RECIPES.recipeBuilder("opv_universal_circuit")
                     .inputItems(CustomTags.OpV_CIRCUITS)
-                    .outputItems(OpV_UNIVERSAL_CIRCUIT)
+                    .outputItems(Items.UNIVERSAL_CIRCUITS[GTValues.OpV])
                     .duration(20)
                     .EUt(GTValues.VA[GTValues.LV])
                     .circuitMeta(10)
                     .save(provider);
-
         }
     }
 }
