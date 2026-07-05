35af168ee8e68530916a2f9b1d088aea00456d65
Usuario <therealxkorpion@gmail.com>
2026-07-05 16:24:44 -0300

    Configuration module

--- src/main/java/com/argxment/extendedfeatures/init/Items.java
+++ src/main/java/com/argxment/extendedfeatures/init/Items.java
@@ -1,13 +1,19 @@
 package com.argxment.extendedfeatures.init;
 
 import com.argxment.extendedfeatures.ExtendedFeaturesCore;
+import com.argxment.extendedfeatures.config.EFModulesConfig;
 
+import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.data.recipe.CustomTags;
+import com.gregtechceu.gtceu.GTCEu;
 import com.tterrag.registrate.util.entry.ItemEntry;
 
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
-import java.util.List;
 import net.minecraftforge.eventbus.api.IEventBus;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static com.argxment.extendedfeatures.ExtendedFeaturesCore.ExtraFeaturesRegister;
 
@@ -19,113 +25,58 @@ public class Items {
 
     // Huge thanks to witherschat (monifactory contributor) for allowing me to use the universal circuit textures
 
-    public static final ItemEntry<Item> ULV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("ulv_universal_circuit", Item::new)
-            .lang("§8ULV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/ulv_universal_circuit")))
-            .tag(CustomTags.ULV_CIRCUITS)
-            .register();
+    @SuppressWarnings("unchecked")
+    public static final ItemEntry<Item>[] UNIVERSAL_CIRCUITS = new ItemEntry[GTValues.TIER_COUNT];
 
-    public static final ItemEntry<Item> LV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("lv_universal_circuit", Item::new)
-            .lang("§7LV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/lv_universal_circuit")))
-            .tag(CustomTags.LV_CIRCUITS)
-            .register();
+    private static final Object[][] CIRCUIT_DATA = {
+            { GTValues.ULV, "ulv_universal_circuit", "§8ULV §rUniversal Circuit",   CustomTags.ULV_CIRCUITS },
+            { GTValues.LV,  "lv_universal_circuit",  "§7LV §rUniversal Circuit",    CustomTags.LV_CIRCUITS },
+            { GTValues.MV,  "mv_universal_circuit",  "§bMV §rUniversal Circuit",    CustomTags.MV_CIRCUITS },
+            { GTValues.HV,  "hv_universal_circuit",  "§6HV §rUniversal Circuit",    CustomTags.HV_CIRCUITS },
+            { GTValues.EV,  "ev_universal_circuit",  "§5EV §rUniversal Circuit",    CustomTags.EV_CIRCUITS },
+            { GTValues.IV,  "iv_universal_circuit",  "§9IV §rUniversal Circuit",    CustomTags.IV_CIRCUITS },
+            { GTValues.LuV, "luv_universal_circuit", "§dLuV §rUniversal Circuit",   CustomTags.LuV_CIRCUITS },
+            { GTValues.ZPM, "zpm_universal_circuit", "§cZPM §rUniversal Circuit",   CustomTags.ZPM_CIRCUITS },
+            { GTValues.UV,  "uv_universal_circuit",  "§3UV §rUniversal Circuit",    CustomTags.UV_CIRCUITS },
+            { GTValues.UHV, "uhv_universal_circuit", "§4UHV §rUniversal Circuit",   CustomTags.UHV_CIRCUITS },
+            { GTValues.UEV, "uev_universal_circuit", "§2UEV §rUniversal Circuit",   CustomTags.UEV_CIRCUITS },
+            { GTValues.UIV, "uiv_universal_circuit", "§aUIV §rUniversal Circuit",   CustomTags.UIV_CIRCUITS },
+            { GTValues.UXV, "uxv_universal_circuit", "§eUXV §rUniversal Circuit",   CustomTags.UXV_CIRCUITS },
+            { GTValues.OpV, "opv_universal_circuit", "§9OpV §rUniversal Circuit", CustomTags.OpV_CIRCUITS },
+    };
 
-    public static final ItemEntry<Item> MV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("mv_universal_circuit", Item::new)
-            .lang("§bMV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/mv_universal_circuit")))
-            .tag(CustomTags.MV_CIRCUITS)
-            .register();
+    static {
+        if (EFModulesConfig.INSTANCE.features.universalCircuits || GTCEu.isDataGen()) {
+            for (Object[] data : CIRCUIT_DATA) {
+                int tier = (int) data[0];
+                String registryName = (String) data[1];
+                String displayName = (String) data[2];
+                @SuppressWarnings("unchecked")
+                TagKey<Item> tag = (TagKey<Item>) data[3];
 
-    public static final ItemEntry<Item> HV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("hv_universal_circuit", Item::new)
-            .lang("§6HV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/hv_universal_circuit")))
-            .tag(CustomTags.HV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> EV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("ev_universal_circuit", Item::new)
-            .lang("§5EV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/ev_universal_circuit")))
-            .tag(CustomTags.EV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> IV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("iv_universal_circuit", Item::new)
-            .lang("§9IV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/iv_universal_circuit")))
-            .tag(CustomTags.IV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> LuV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("luv_universal_circuit", Item::new)
-            .lang("§dLuV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/luv_universal_circuit")))
-            .tag(CustomTags.LuV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> ZPM_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("zpm_universal_circuit", Item::new)
-            .lang("§cZPM §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/zpm_universal_circuit")))
-            .tag(CustomTags.ZPM_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> UV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("uv_universal_circuit", Item::new)
-            .lang("§3UV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/uv_universal_circuit")))
-            .tag(CustomTags.UV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> UHV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("uhv_universal_circuit", Item::new)
-            .lang("§4UHV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/uhv_universal_circuit")))
-            .tag(CustomTags.UHV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> UEV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("uev_universal_circuit", Item::new)
-            .lang("§2UEV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/uev_universal_circuit")))
-            .tag(CustomTags.UEV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> UIV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("uiv_universal_circuit", Item::new)
-            .lang("§aUIV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/uiv_universal_circuit")))
-            .tag(CustomTags.UIV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> UXV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("uxv_universal_circuit", Item::new)
-            .lang("§eUXV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/uxv_universal_circuit")))
-            .tag(CustomTags.UXV_CIRCUITS)
-            .register();
-
-    public static final ItemEntry<Item> OpV_UNIVERSAL_CIRCUIT = ExtraFeaturesRegister
-            .item("opv_universal_circuit", Item::new)
-            .lang("§l§9OpV §rUniversal Circuit")
-            .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/opv_universal_circuit")))
-            .tag(CustomTags.OpV_CIRCUITS)
-            .register();
+                UNIVERSAL_CIRCUITS[tier] = ExtraFeaturesRegister
+                        .item(registryName, Item::new)
+                        .lang(displayName)
+                        .model((ctx, prov) -> prov.generated(ctx, prov.modLoc("item/universal/" + registryName)))
+                        .tag(tag)
+                        .register();
+            }
+        }
+    }
 
     // Addon "isHighTier" filter
     public static List<ItemEntry<Item>> getHighTierCircuits() {
-        return List.of(
-                UHV_UNIVERSAL_CIRCUIT,
-                UEV_UNIVERSAL_CIRCUIT,
-                UIV_UNIVERSAL_CIRCUIT,
-                UXV_UNIVERSAL_CIRCUIT,
-                OpV_UNIVERSAL_CIRCUIT
-        );
+        int[] highTiers = { GTValues.UHV, GTValues.UEV, GTValues.UIV, GTValues.UXV, GTValues.OpV };
+        List<ItemEntry<Item>> result = new ArrayList<>();
+
+        for (int tier : highTiers) {
+            ItemEntry<Item> entry = UNIVERSAL_CIRCUITS[tier];
+            if (entry != null) {
+                result.add(entry);
+            }
+        }
+
+        return result;
     }
 
     public static void register(IEventBus eventBus) {}
