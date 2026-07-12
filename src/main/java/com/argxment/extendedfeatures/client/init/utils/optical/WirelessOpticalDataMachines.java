552fa2adf8d94b568e8ed6567e2e7c2f3388b4f1
Usuario <therealxkorpion@gmail.com>
2026-07-12 19:31:09 -0300

    Fixed spotless mess again!

--- src/main/java/com/argxment/extendedfeatures/client/init/utils/optical/WirelessOpticalDataMachines.java
+++ src/main/java/com/argxment/extendedfeatures/client/init/utils/optical/WirelessOpticalDataMachines.java
@@ -25,58 +25,49 @@ public class WirelessOpticalDataMachines {
 
     static {
         if (EFConfig.INSTANCE.Wireless.wirelessUtils || GTCEu.isDataGen()) {
-            LUV_WIRELESS_TRANSMITTER = registerHatch(
-                    "luv_wireless_data_transmitter", "LuV Wireless Optical Transmission Hatch",
-                    LuV, true)
+            LUV_WIRELESS_TRANSMITTER = registerHatch("luv_wireless_data_transmitter",
+                    "LuV Wireless Optical Transmission Hatch", LuV, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 16),
-                            Component.translatable(
-                                    "extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 4),
+                            Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 4),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.scan"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
-            LUV_WIRELESS_RECEIVER = registerHatch(
-                    "luv_wireless_data_receiver", "LuV Wireless Optical Reception Hatch",
-                    LuV, false)
+            LUV_WIRELESS_RECEIVER = registerHatch("luv_wireless_data_receiver",
+                    "LuV Wireless Optical Reception Hatch", LuV, false)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.receiver"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
-            ZPM_WIRELESS_TRANSMITTER = registerHatch(
-                    "zpm_wireless_data_transmitter", "ZPM Wireless Optical Transmission Hatch",
-                    ZPM, true)
+            ZPM_WIRELESS_TRANSMITTER = registerHatch("zpm_wireless_data_transmitter",
+                    "ZPM Wireless Optical Transmission Hatch", ZPM, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 24),
-                            Component.translatable(
-                                    "extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 8),
+                            Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 8),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.scan"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
-            ZPM_WIRELESS_RECEIVER = registerHatch(
-                    "zpm_wireless_data_receiver", "ZPM Wireless Optical Reception Hatch",
-                    ZPM, false)
+            ZPM_WIRELESS_RECEIVER = registerHatch("zpm_wireless_data_receiver",
+                    "ZPM Wireless Optical Reception Hatch", ZPM, false)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.receiver"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
-            UV_WIRELESS_TRANSMITTER = registerHatch(
-                    "uv_wireless_data_transmitter", "UV Wireless Optical Transmission Hatch",
-                    UV, true)
+            UV_WIRELESS_TRANSMITTER = registerHatch("uv_wireless_data_transmitter",
+                    "UV Wireless Optical Transmission Hatch", UV, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 32),
-                            Component.translatable(
-                                    "extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 16),
+                            Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 16),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.scan"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
-            UV_WIRELESS_RECEIVER = registerHatch(
-                    "uv_wireless_data_receiver", "UV Wireless Optical Reception Hatch",
-                    UV, false)
+            UV_WIRELESS_RECEIVER = registerHatch("uv_wireless_data_receiver",
+                    "UV Wireless Optical Reception Hatch", UV, false)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.receiver"),
                             Component.translatable("gtceu.part_sharing.disabled"))
@@ -84,8 +75,7 @@ public class WirelessOpticalDataMachines {
         }
     }
 
-    private static MachineBuilder<MachineDefinition, ?> registerHatch(String name, String displayName, int tier,
-                                                                      boolean isTransmitter) {
+    private static MachineBuilder<MachineDefinition, ?> registerHatch(String name, String displayName, int tier, boolean isTransmitter) {
         return ExtendedFeaturesRegister
                 .machine(name, (holder) -> new WirelessOpticalDataHatchMachine(holder, isTransmitter, tier))
                 .langValue(displayName)
@@ -95,9 +85,12 @@ public class WirelessOpticalDataMachines {
                  * if -> IsTransmitter = true: Register a WIRELESS_OPTICAL_TRASMITTER
                  * Or else, IsTransmitter = false -> Register a WIRELESS_OPTICAL_RECEIVER
                  */
-                .abilities(isTransmitter ? WirelessAbilities.WIRELESS_OPTICAL_TRANSMITTER :
-                        WirelessAbilities.WIRELESS_OPTICAL_RECEIVER)
-                .overlayTieredHullModel(isTransmitter ? "wireless_optical_transmission" : "wireless_optical_reception");
+                .abilities(isTransmitter
+                        ? WirelessAbilities.WIRELESS_OPTICAL_TRANSMITTER
+                        : WirelessAbilities.WIRELESS_OPTICAL_RECEIVER)
+                .overlayTieredHullModel(isTransmitter
+                        ? "wireless_optical_transmission"
+                        : "wireless_optical_reception");
     }
 
     public static void init() {}
