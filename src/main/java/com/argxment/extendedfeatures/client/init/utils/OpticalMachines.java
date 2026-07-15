e3099402a0a61dc480ee616eaab1ac257ccf79b2
Usuario <therealxkorpion@gmail.com>
2026-07-15 11:21:58 -0300

    random changes

--- src/main/java/com/argxment/extendedfeatures/client/init/utils/OpticalMachines.java
+++ src/main/java/com/argxment/extendedfeatures/client/init/utils/OpticalMachines.java
@@ -17,7 +17,7 @@ import static com.argxment.extendedfeatures.ExtendedFeaturesCore.ExtendedFeature
 import static com.gregtechceu.gtceu.api.GTValues.*;
 import static com.gregtechceu.gtceu.api.machine.property.GTMachineModelProperties.IS_FORMED;
 
-public class ExpandedDataHatches {
+public class OpticalMachines {
 
     // Expanded Data Access Hatches
     public static MachineDefinition ZPM_DATA_ACCESS_HATCH = null;
@@ -39,7 +39,8 @@ public class ExpandedDataHatches {
                     .modelProperty(IS_FORMED, false)
                     .tooltips(
                             Component.translatable("gtceu.machine.data_access_hatch.tooltip.0"),
-                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 36))
+                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 36),
+                            Component.translatable("gtceu.part_sharing.disabled"))
                     .overlayTieredHullModel("expanded_data_access_hatch")
                     .register();
 
@@ -56,7 +57,8 @@ public class ExpandedDataHatches {
                     .modelProperty(IS_FORMED, false)
                     .tooltips(
                             Component.translatable("gtceu.machine.data_access_hatch.tooltip.0"),
-                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 49))
+                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 49),
+                            Component.translatable("gtceu.part_sharing.disabled"))
                     .overlayTieredHullModel("expanded_data_access_hatch")
                     .register();
 
@@ -73,7 +75,8 @@ public class ExpandedDataHatches {
                     .modelProperty(IS_FORMED, false)
                     .tooltips(
                             Component.translatable("gtceu.machine.data_access_hatch.tooltip.0"),
-                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 64))
+                            Component.translatable("gtceu.machine.data_access_hatch.tooltip.1", 64),
+                            Component.translatable("gtceu.part_sharing.disabled"))
                     .overlayTieredHullModel("expanded_data_access_hatch")
                     .register();
         }
@@ -92,7 +95,7 @@ public class ExpandedDataHatches {
     static {
         if (EFConfig.INSTANCE.Wireless.wirelessUtils || GTCEu.isDataGen()) {
             LUV_WIRELESS_TRANSMITTER = registerHatch("luv_wireless_data_transmitter",
-                    "LuV Wireless Optical Transmission Hatch", LuV, true)
+                    "LuV Wireless Transmission Hatch", LuV, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 16),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 4),
@@ -108,7 +111,7 @@ public class ExpandedDataHatches {
                     .register();
 
             ZPM_WIRELESS_TRANSMITTER = registerHatch("zpm_wireless_data_transmitter",
-                    "ZPM Wireless Optical Transmission Hatch", ZPM, true)
+                    "ZPM Wireless Transmission Hatch", ZPM, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 24),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 8),
@@ -117,14 +120,14 @@ public class ExpandedDataHatches {
                     .register();
 
             ZPM_WIRELESS_RECEIVER = registerHatch("zpm_wireless_data_receiver",
-                    "ZPM Wireless Optical Reception Hatch", ZPM, false)
+                    "ZPM Wireless Reception Hatch", ZPM, false)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.receiver"),
                             Component.translatable("gtceu.part_sharing.disabled"))
                     .register();
 
             UV_WIRELESS_TRANSMITTER = registerHatch("uv_wireless_data_transmitter",
-                    "UV Wireless Optical Transmission Hatch", UV, true)
+                    "UV Wireless Transmission Hatch", UV, true)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.range", 32),
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", 16),
@@ -133,7 +136,7 @@ public class ExpandedDataHatches {
                     .register();
 
             UV_WIRELESS_RECEIVER = registerHatch("uv_wireless_data_receiver",
-                    "UV Wireless Optical Reception Hatch", UV, false)
+                    "UV Wireless Reception Hatch", UV, false)
                     .tooltips(
                             Component.translatable("extendedfeatures.machine.wireless_optical_hatch.tooltip.receiver"),
                             Component.translatable("gtceu.part_sharing.disabled"))
@@ -155,8 +158,9 @@ public class ExpandedDataHatches {
                         ? WirelessAbilities.WIRELESS_OPTICAL_TRANSMITTER
                         : WirelessAbilities.WIRELESS_OPTICAL_RECEIVER)
                 .overlayTieredHullModel(isTransmitter
-                        ? "wireless_optical_transmission"
-                        : "wireless_optical_reception");
+                        ? "wireless_transmitter"
+                        : "wireless_receiver"
+                );
     }
 
     public static void init() {}
