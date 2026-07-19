07eaddff42fb9a17b0695e9a8f89e9cfb2a5a8a7
Usuario <therealxkorpion@gmail.com>
2026-07-19 13:43:20 -0300

    Config refactor

--- src/main/java/com/extendedfeatures/client/integrations/Configuration/EFConfig.java
+++ src/main/java/com/extendedfeatures/client/integrations/Configuration/EFConfig.java
@@ -19,83 +19,123 @@ public class EFConfig {
     }
 
     @Configurable
-    @Configurable.Comment("Configuration values for Multiblocks")
+    @Configurable.Comment("Configuration Toggles for Multiblocks")
     public MultiblocksToggles Multiblocks = new MultiblocksToggles();
 
     @Configurable
-    @Configurable.Comment("Configuration Values for Machines and Items")
-    public OtherToggles Miscellaneous = new OtherToggles();
+    @Configurable.Comment("Configuration Toggles for Optical Features")
+    public OpticalToggle OpticalMachines = new OpticalToggle();
 
     @Configurable
-    @Configurable.Comment("Special section for Wireless Machines")
-    public WirelessToggle Wireless = new WirelessToggle();
+    @Configurable.Comment({ "Whether the Universal Circuits are Enabled." })
+    public boolean UniversalCircuits = true;
 
     public static class MultiblocksToggles {
 
         @Configurable
-        @Configurable.Comment({ "Whether the Robust Alloy Materializer is Enabled." })
-        public boolean RamEnabled = true;
+        @Configurable.Comment({
+                "Whether the Robust Alloy Materializer is Enabled",
+                "Default = True"
+        })
+        public boolean RobustAlloyMaterializer = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Large Cracking Machine is Enabled." })
-        public boolean LcmEnabled = true;
+        @Configurable.Comment({
+                "Whether the Large Cracking Machine is Enabled",
+                "Default = True"
+        })
+        public boolean LargeCrackingMachine = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Enlarged Reaction Chamber is Enabled." })
-        public boolean ErcEnabled = true;
+        @Configurable.Comment({
+                "Whether the Synthesis Vessel is Enabled",
+                "Default = True"
+        })
+        public boolean SynthesisVessel = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Large Pyrolysis Oven is Enabled." })
-        public boolean LpoEnabled = true;
+        @Configurable.Comment({
+                "Whether the Large Pyrolysis Oven is Enabled",
+                "Default = True"
+        })
+        public boolean LargePyrolysisOven = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Advanced Fusion Reactors are Enabled." })
-        public boolean AfrEnabled = true;
+        @Configurable.Comment({
+                "Whether the Advanced Fusion Reactors are Enabled",
+                "Default = True"
+        })
+        public boolean AdvancedFusionReactors = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Compact Assembly Line is Enabled." })
-        public boolean CalEnabled = true;
+        @Configurable.Comment({
+                "Whether the Compact Assembly Line is Enabled",
+                "Tip: Controller recipe should contain 4 Assembly Line controllers",
+                "Default = True"
+        })
+        public boolean CompactAssemblyLine = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Rock Processing Facility is Enabled." })
-        public boolean RpfEnabled = true;
+        @Configurable.Comment({
+                "Whether the Rock Processing Plant is Enabled",
+                "Tip: Controller recipe should contain a macerator, a centrifuge and an electrolyzer",
+                "Default = True"
+        })
+        public boolean RockProcessingPlant = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Industrial Greenhouse is Enabled." })
-        public boolean IghEnabled = true;
+        @Configurable.Comment({
+                "Whether the Industrial Greenhouse is Enabled",
+                "Default = True"
+        })
+        public boolean IndustrialGreenhouse = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Tree Growing Chamber is Enabled." })
-        public boolean TgcEnabled = true;
+        @Configurable.Comment({
+                "Whether the Tree Growing Chamber is Enabled",
+                "Default = True"
+        })
+        public boolean TreeGrowingChamber = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Disassembler is Enabled." })
-        public boolean DaEnabled = true;
+        @Configurable.Comment({
+                "Whether the Disassembler is enabled",
+                "§cWarning: §rUniversal Circuits MUST be enabled, otherwise it could lead to some issues with recipes",
+                "Default = True"
+        })
+        public boolean Disassembler = true;
 
-        @Configurable
-        @Configurable.Comment({ "Whether the Expanded Data Bank is Enabled." })
-        public boolean EdaEnabled = true;
-
-        @Configurable
-        @Configurable.Comment({ "Whether the Cloud Transmission Database is Enabled." })
-        public boolean CtdEnabled = true;
     }
 
-    public static class OtherToggles {
+    public static class OpticalToggle {
 
         @Configurable
-        @Configurable.Comment({ "Whether the Universal Circuits are Enabled." })
-        public boolean universalCircuits = true;
+        @Configurable.Comment({
+                "Whether the Expanded Data Access Hatches are Enabled",
+                "Default = True"
+        })
+        public boolean ExpandedDataAccessHatches = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Expanded Data Access Hatches are Enabled." })
-        public boolean expandedDataAccessHatches = true;
-    }
-
-    public static class WirelessToggle {
+        @Configurable.Comment({
+                "Whether the Wireless Optical Hatches & Cloud Transmission Database are Enabled",
+                "Default = True"
+        })
+        public boolean WirelessOptical = true;
 
         @Configurable
-        @Configurable.Comment({ "Whether the Wireless Optical Hatches & Cloud Transmission Database are Enabled." })
-        public boolean wirelessUtils = true;
+        @Configurable.Comment({
+                "Whether the Expanded Data Bank is Enabled",
+                "Default = True"
+        })
+        public boolean ExpandedDatabank = true;
+
+        @Configurable
+        @Configurable.Comment({
+                "Whether the Optical Tranmission Network is Enabled",
+                "Default = True"
+        })
+        public boolean OpticalTransmissionNetwork = true;
+
     }
 }
