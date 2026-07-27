c4a008231ae8140726b085af180874f4fe9c37d8
Usuario <therealxkorpion@gmail.com>
2026-07-27 13:07:42 -0300

    Removed tiered parallel

--- src/main/java/com/extendedfeatures/init/utils/RecipeModifiers.java
+++ src/main/java/com/extendedfeatures/init/utils/RecipeModifiers.java
@@ -1,49 +1,29 @@
 package com.extendedfeatures.init.utils;
 
-import com.gregtechceu.gtceu.api.GTValues;
 import com.gregtechceu.gtceu.api.recipe.content.ContentModifier;
-import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
-import com.gregtechceu.gtceu.api.recipe.modifier.ParallelLogic;
-import com.gregtechceu.gtceu.api.recipe.modifier.RecipeModifier;
+import com.gregtechceu.gtceu.api.recipe.modifier.*;
 
-import java.util.function.IntFunction;
+import java.util.function.*;
 
 public class RecipeModifiers {
 
-    private static final int MAX_CUSTOM_PARALLEL = 2147483647;
+    private static final int MAX_CUSTOM_PARALLEL = 2147483647; // 2^32-1
 
     public static final IntFunction<RecipeModifier> CUSTOM_PARALLEL = parallels -> {
         if (parallels == 1) return RecipeModifier.NO_MODIFIER;
 
         return (machine, recipe) -> {
+
             int achievable = ParallelLogic.getParallelAmountWithoutEU(machine, recipe, parallels);
+
             if (achievable <= 1) return ModifierFunction.IDENTITY;
             return ModifierFunction.builder()
                     .modifyAllContents(ContentModifier.multiplier(achievable))
                     .durationMultiplier(2)
+                    .eutMultiplier(1)
                     .parallels(achievable)
                     .build();
         };
     };
 
-    public static final RecipeModifier TIERED_PARALLEL = (machine, recipe) -> {
-        int tier = machine.self().getDefinition().getTier();
-        int parallels = switch (tier) {
-            case GTValues.LuV -> 4;
-            case GTValues.ZPM -> 8;
-            case GTValues.UV -> 16;
-            default -> 1;
-        };
-
-        if (parallels <= 1) return ModifierFunction.IDENTITY;
-
-        int achievable = ParallelLogic.getParallelAmountWithoutEU(machine, recipe, parallels);
-        if (achievable <= 1) return ModifierFunction.IDENTITY;
-
-        return ModifierFunction.builder()
-                .modifyAllContents(ContentModifier.multiplier(achievable))
-                .durationMultiplier(1)
-                .parallels(achievable)
-                .build();
-    };
 }
