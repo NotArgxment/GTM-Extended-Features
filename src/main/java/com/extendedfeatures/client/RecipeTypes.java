5f5e4a9349d86b42d3cec6432a7642813892d850
Argxment <therealxkorpion@gmail.com>
2026-07-31 15:28:12 -0300

    Added comment for RecipeType builder

--- src/main/java/com/extendedfeatures/client/RecipeTypes.java
+++ src/main/java/com/extendedfeatures/client/RecipeTypes.java
@@ -81,6 +81,7 @@ public class RecipeTypes {
 
     }
 
+    // Addon's own RecipeType - otherwise they register under GTCEu Namespace
     public static GTRecipeType register(String name, String group, RecipeType<?>... proxyRecipes) {
         ResourceLocation id = ExtendedFeaturesCore.id(name);
 
