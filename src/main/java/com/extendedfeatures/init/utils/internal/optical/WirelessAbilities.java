ec8bb0a15c11260078c8a35007719b47e7505758
Usuario <therealxkorpion@gmail.com>
2026-08-02 22:00:55 -0300

    Renamed following 46ee4399

--- src/main/java/com/extendedfeatures/init/utils/internal/optical/WirelessAbilities.java
+++ src/main/java/com/extendedfeatures/init/utils/internal/optical/WirelessAbilities.java
@@ -4,8 +4,8 @@ import com.gregtechceu.gtceu.api.machine.multiblock.PartAbility;
 
 public class WirelessAbilities {
 
-    public static final PartAbility WIRELESS_OPTICAL_TRANSMITTER = new PartAbility("wireless_optical_transmitter");
-    public static final PartAbility WIRELESS_OPTICAL_RECEIVER = new PartAbility("wireless_optical_receiver");
+    public static final PartAbility WIRELESS_OPTICAL_TRANSMISSOR = new PartAbility("wireless_optical_transmissor");
+    public static final PartAbility WIRELESS_OPTICAL_RECEPTOR = new PartAbility("wireless_optical_receptor");
 
     private WirelessAbilities() {}
 }
