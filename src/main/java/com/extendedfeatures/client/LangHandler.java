1fca832e23da1f58d892f95697962d8fef8b3c07
Usuario <therealxkorpion@gmail.com>
2026-08-02 21:54:05 -0300

    General fixes

--- src/main/java/com/extendedfeatures/client/LangHandler.java
+++ src/main/java/com/extendedfeatures/client/LangHandler.java
@@ -12,18 +12,19 @@ public class LangHandler {
     }
 
     private static void Common(RegistrateLangProvider provider) {
+
         // Regular item/block lang
         replace(provider, "block.extendedfeatures.robust_alloy_materializer", "Robust Alloy Materializer [RAM]");
-        replace(provider, "block.extendedfeatures.large_cracking_machine", "Large Cracker Machine [LCM]");
-        replace(provider, "block.extendedfeatures.synthesys_vessel", "Synthesys Vessel [SyVe]");
+        replace(provider, "block.extendedfeatures.large_cracking_machine", "Large Cracking Machine [LCM]");
+        replace(provider, "block.extendedfeatures.synthesis_vessel", "Synthesis Vessel [SyVe]");
         replace(provider, "block.extendedfeatures.large_pyrolysis_oven", "Large Pyrolysis Oven [LPO]");
         replace(provider, "block.extendedfeatures.compact_assembly_line", "Compact Assembly Line [CAL]");
         replace(provider, "block.extendedfeatures.rock_processing_plant", "Rock Processing Plant [RPP]");
         replace(provider, "block.extendedfeatures.industrial_greenhouse", "Industrial Greenhouse [IGh]");
         replace(provider, "block.extendedfeatures.tree_growing_chamber", "Tree Growing Chamber [TGCh]");
-        replace(provider, "block.extendedfeatures.zpm_data_access_hatch", "ZPM Data Access Hatch");
-        replace(provider, "block.extendedfeatures.uv_data_access_hatch", "UV Data Access Hatch");
-        replace(provider, "block.extendedfeatures.uhv_data_access_hatch", "UHV Data Access Hatch");
+        replace(provider, "block.extendedfeatures.zpm_data_access_hatch", "Elite Data Access Hatch");
+        replace(provider, "block.extendedfeatures.uv_data_access_hatch", "Ultimate Data Access Hatch");
+        replace(provider, "block.extendedfeatures.uhv_data_access_hatch", "Epic Data Access Hatch");
 
         // Recipe types lang
         replace(provider, "extendedfeatures.greenhouse_wood_recipes", "Tree Growing");
@@ -36,12 +37,10 @@ public class LangHandler {
         replace(provider, "config.screen.extendedfeatures", "§7Mod Configuration §4(Restart to Apply Changes)");
         replace(provider, "config.extendedfeatures.option.Multiblocks", "§7Multiblocks");
         replace(provider, "config.extendedfeatures.option.OpticalMachines", "§7Optical Features");
-        replace(provider, "config.extendedfeatures.option.UniversalCircuits", "§7Universal Circuits");
         replace(provider, "config.extendedfeatures.option.RobustAlloyMaterializer", "§7Robust Alloy Materializer");
         replace(provider, "config.extendedfeatures.option.LargeCrackingMachine", "§7Large Cracking Machine");
-        replace(provider, "config.extendedfeatures.option.SynthesisVessel", "§7Enlarged Reaction Chamber");
+        replace(provider, "config.extendedfeatures.option.SynthesisVessel", "§7Synthesis Vessel");
         replace(provider, "config.extendedfeatures.option.LargePyrolysisOven", "§7Large Pyrolysis Oven");
-        replace(provider, "config.extendedfeatures.option.AdvancedFusionReactors", "§7Advanced Fusion Reactors");
         replace(provider, "config.extendedfeatures.option.CompactAssemblyLine", "§7Compact Assembly Line");
         replace(provider, "config.extendedfeatures.option.RockProcessingPlant", "§7Rock Processing Plant");
         replace(provider, "config.extendedfeatures.option.IndustrialGreenhouse", "§7Industrial Greenhouse");
@@ -50,8 +49,8 @@ public class LangHandler {
         replace(provider, "config.extendedfeatures.option.ExpandedDatabank", "§7Expanded Data Bank");
         replace(provider, "config.extendedfeatures.option.UniversalCircuits", "§7Universal Circuits");
         replace(provider, "config.extendedfeatures.option.ExpandedDataAccessHatches", "§7Expanded Data Access Hatches");
-        replace(provider, "config.extendedfeatures.option.WirelessOptical", "§7Wireless Optical Tranmission/Reception Hatches");
-        replace(provider, "config.extendedfeatures.option.OpticalTransmissionNetwork", "§7Optical Transmission Network ");
+        replace(provider, "config.extendedfeatures.option.WirelessOptical", "§7Wireless Optical Tranmisssors/Receptors");
+        replace(provider, "config.extendedfeatures.option.CloudTransmissionDatabase", "§7Cloud Transmission Database");
 
         // Optical
         replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.range", "Scan range: §f%s blocks§7 (right-click with an empty hand)");
@@ -66,19 +65,21 @@ public class LangHandler {
         // Jade integration
         replace(provider, "config.jade.plugin_extendedfeatures.wireless_optical_hatch", "Wireless Optical Info");
         replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_data_hatches", "Linked Data Access Hatches: %s");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_receivers_header", "Linked Wireless Optical Receivers:");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.receiver_entry", "   - Receiver %s: %s");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.no_receivers", "   - Not linked");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_receptors_header", "Linked Wireless Optical Receivers:");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.receptors_entry", "   - Receiver %s: %s");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.no_receptors", "   - Not linked");
+
     }
 
     private static void Tooltips(RegistrateLangProvider provider) {
+
         provider.add("extendedfeatures.compact_assembly_line.tooltip.0", "§7This machine performs the work of the Assembly Line without §9Ordered Inputs");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.1", "§fAllows §bone §fenergy hatch");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.2", "§fRuns §b4 §frecipes in parallel");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.3", "§fOnly works using");
 
-        provider.add("extendedfeatures.synthesys_vessel.tooltip.0", "§fEven larger Black Box Reactor");
-        provider.add("extendedfeatures.synthesys_vessel.tooltip.1", "§7This machine is an improved version of the Large Chemical reactor capable of running ");
+        provider.add("extendedfeatures.synthesis_vessel.tooltip.0", "§fEven larger Black Box Reactor");
+        provider.add("extendedfeatures.synthesis_vessel.tooltip.1", "§7This machine is an improved version of the Large Chemical reactor capable of performing whole chemical lines in just 1 step");
 
         provider.add("extendedfeatures.rock_processing_plant.tooltip.0", "§7An all in one processing line, turns the rocks you normally get from the rock breaker into their direct processed outputs");
 
@@ -88,12 +89,11 @@ public class LangHandler {
         provider.add("extendedfeatures.disassembler.tooltip.0", "§7Allows any type of §emachine/controller §7to be recycled, returning what was used for that recipe");
         provider.add("extendedfeatures.disassembler.tooltip.1", "§fEach tier of machine needs the same energy hatch of that tier in order to work");
 
-        provider.add("extendedfeatures.expanded_databank_tootip.1", "§fAllows 16"); // inside a tooltip builer, followed by "extendedfeatures.styled.tooltip.5"
+        provider.add("extendedfeatures.expanded_databank_tootip.1", "§fAllows 16 independent Optical Transmission connections");
 
         provider.add("extendedfeatures.cloud_transmission_database.tooltip.1", "§fYour personal Data Center");
-        provider.add("extendedfeatures.cloud_transmission_database.tooltip.2", "§fThis machine is a variant of the regular Data Bank capable of transmitting data using");
-        provider.add("extendedfeatures.cloud_transmission_database.tooltip.3", "§fNeeds exactly 1 Data Access Hatch to form");
-        provider.add("extendedfeatures.cloud_transmission_database.tooltip.4", "§fOnly allows");
+        provider.add("extendedfeatures.cloud_transmission_database.tooltip.2", "§fNeeds exactly 1 Data Access Hatch to form");
+        provider.add("extendedfeatures.cloud_transmission_database.tooltip.3", "§fOnly allows"); // followed by "extendedfeatures.styled.tooltip.4"
 
         provider.add("extendedfeatures.large_air_collector.tooltip.0", "§7A Bigger Gas Collector");
         provider.add("extendedfeatures.air_processing_machine.tooltip.0", "§7A machine capable of processing the air from the environment to get valuable products from it");
@@ -102,8 +102,8 @@ public class LangHandler {
         provider.add("extendedfeatures.regular.tooltip.2", "§fand");
         provider.add("extendedfeatures.styled.tooltip.1", " Laser Hatches ");
         provider.add("extendedfeatures.styled.tooltip.2", " Parallel Hatches ");
-        provider.add("extendedfeatures.styled.tooltip.3", " Wireless Optical Reception Hatches ");
-        provider.add("extendedfeatures.styled.tooltip.4", " Wireless Optical Transmission Hatches ");
-        provider.add("extendedfeatures.styled.tooltip.5", " Optical Transmission Hatches ");
+        provider.add("extendedfeatures.styled.tooltip.3", " Wireless Optical Receptors ");
+        provider.add("extendedfeatures.styled.tooltip.4", " Wireless Optical Transmissors ");
+
     }
 }
