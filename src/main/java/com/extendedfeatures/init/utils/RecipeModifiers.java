b8bbd586dc4b8e687fbe6bade60ec57d69fd6a88
Usuario <therealxkorpion@gmail.com>
2026-08-02 21:58:55 -0300

    Changed how custom parallels are used: "CUSTOM_PARALLEL.apply(n)" to "CUSTOM_PARALLEL(n)", removed EUt multiplier and useless int for MAX parallels

--- src/main/java/com/extendedfeatures/init/utils/RecipeModifiers.java
+++ src/main/java/com/extendedfeatures/init/utils/RecipeModifiers.java
@@ -1,29 +1,25 @@
 package com.extendedfeatures.init.utils;
 
+import com.gregtechceu.gtceu.api.machine.MetaMachine;
+import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.content.ContentModifier;
 import com.gregtechceu.gtceu.api.recipe.modifier.*;
 
-import java.util.function.*;
-
 public class RecipeModifiers {
 
-    private static final int MAX_CUSTOM_PARALLEL = 2147483647; // 2^32-1
-
-    public static final IntFunction<RecipeModifier> CUSTOM_PARALLEL = parallels -> {
+    public static RecipeModifier CUSTOM_PARALLEL(int parallels) {
         if (parallels == 1) return RecipeModifier.NO_MODIFIER;
 
-        return (machine, recipe) -> {
-
+        return (MetaMachine machine, GTRecipe recipe) -> {
             int achievable = ParallelLogic.getParallelAmountWithoutEU(machine, recipe, parallels);
 
             if (achievable <= 1) return ModifierFunction.IDENTITY;
             return ModifierFunction.builder()
                     .modifyAllContents(ContentModifier.multiplier(achievable))
                     .durationMultiplier(2)
-                    .eutMultiplier(1)
                     .parallels(achievable)
                     .build();
         };
-    };
+    }
 
 }
