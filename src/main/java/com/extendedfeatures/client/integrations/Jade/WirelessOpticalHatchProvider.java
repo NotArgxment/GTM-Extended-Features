a7f0c84200dfa7738f76e0e1ec7e00f1fb331356
Usuario <therealxkorpion@gmail.com>
2026-08-08 12:00:41 -0300

    Miscellaneous changes
    Signed-off-by: Usuario <therealxkorpion@gmail.com>

--- src/main/java/com/extendedfeatures/client/integrations/Jade/WirelessOpticalHatchProvider.java
+++ src/main/java/com/extendedfeatures/client/integrations/Jade/WirelessOpticalHatchProvider.java
@@ -1,7 +1,7 @@
 package com.extendedfeatures.client.integrations.Jade;
 
 import com.extendedfeatures.ExtendedFeaturesCore;
-import com.extendedfeatures.init.utils.internal.optical.WirelessOpticalDataHatchMachine;
+import com.extendedfeatures.init.utils.internal.machines.WirelessOpticalHatch;
 
 import com.gregtechceu.gtceu.api.machine.*;
 
@@ -14,18 +14,17 @@ import net.minecraft.resources.ResourceLocation;
 import snownee.jade.api.*;
 import snownee.jade.api.config.IPluginConfig;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- * Shows, for {@link WirelessOpticalDataHatchMachine} transmitters, the linked physical
- * Data Access Hatches (count) and linked Wireless Optical Receivers (positions) in the Jade tooltip.
+ * Shows, for {@link WirelessOpticalHatch} transmitters, the linked physical
+ * Data Access Hatches (count) and linked Wireless Optical Receptors (positions) in the Jade tooltip.
  */
 public class WirelessOpticalHatchProvider implements IBlockComponentProvider, IServerDataProvider<BlockAccessor> {
 
-    private static final String TAG_IS_TRANSMITTER = "IsTransmitter";
+    private static final String TAG_IS_TRANSMISSOR = "IsTransmitter";
     private static final String TAG_DATA_HATCH_COUNT = "DataHatchCount";
-    private static final String TAG_RECEIVERS = "Receivers";
+    private static final String TAG_RECEPTORS = "Receptors";
 
     @Override
     public ResourceLocation getUid() {
@@ -38,54 +37,54 @@ public class WirelessOpticalHatchProvider implements IBlockComponentProvider, IS
         if (!(blockAccessor.getBlockEntity() instanceof IMachineBlockEntity be)) return;
 
         MetaMachine machine = be.getMetaMachine();
-        if (!(machine instanceof WirelessOpticalDataHatchMachine hatch)) return;
+        if (!(machine instanceof WirelessOpticalHatch hatch)) return;
         if (!hatch.isTransmitter()) return; // only transmitters expose this info
 
-        data.putBoolean(TAG_IS_TRANSMITTER, true);
+        data.putBoolean(TAG_IS_TRANSMISSOR, true);
         data.putInt(TAG_DATA_HATCH_COUNT, hatch.getLinkedDataHatchPositions().size());
 
-        ListTag receivers = new ListTag();
-        for (BlockPos pos : hatch.getLinkedReceiverPositions()) {
-            receivers.add(new IntArrayTag(new int[] { pos.getX(), pos.getY(), pos.getZ() }));
+        ListTag receptors = new ListTag();
+        for (BlockPos pos : hatch.getLinkedReceptorPositions()) {
+            receptors.add(new IntArrayTag(new int[] { pos.getX(), pos.getY(), pos.getZ() }));
         }
-        data.put(TAG_RECEIVERS, receivers);
+        data.put(TAG_RECEPTORS, receptors);
     }
 
     // Client side: build the tooltip
     @Override
     public void appendTooltip(ITooltip tooltip, BlockAccessor blockAccessor, IPluginConfig config) {
         CompoundTag data = blockAccessor.getServerData();
-        if (!data.getBoolean(TAG_IS_TRANSMITTER)) return;
+        if (!data.getBoolean(TAG_IS_TRANSMISSOR)) return;
 
         int dataHatchCount = data.getInt(TAG_DATA_HATCH_COUNT);
         tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.linked_data_hatches",
                 dataHatchCount).withStyle(ChatFormatting.GRAY));
 
-        List<BlockPos> receivers = readReceivers(data.getList(TAG_RECEIVERS, net.minecraft.nbt.Tag.TAG_INT_ARRAY));
+        List<BlockPos> receptors = readReceptors(data.getList(TAG_RECEPTORS, net.minecraft.nbt.Tag.TAG_INT_ARRAY));
 
-        if (receivers.isEmpty()) {
+        if (receptors.isEmpty()) {
             tooltip.add(Component
-                    .translatable("extendedfeatures.jade.wireless_optical_hatch.linked_receivers_header")
+                    .translatable("extendedfeatures.jade.wireless_optical_hatch.linked_receptors_header")
                     .withStyle(ChatFormatting.GRAY));
-            tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.no_receivers")
+            tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.no_receptors")
                     .withStyle(ChatFormatting.DARK_GRAY));
             return;
         }
 
-        tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.linked_receivers_header")
+        tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.linked_receptors_header")
                 .withStyle(ChatFormatting.GRAY));
 
         int index = 1;
-        for (BlockPos pos : receivers) {
+        for (BlockPos pos : receptors) {
             Component coords = Component.literal(pos.getX() + " " + pos.getY() + " " + pos.getZ())
                     .withStyle(ChatFormatting.GOLD);
-            tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.receiver_entry",
+            tooltip.add(Component.translatable("extendedfeatures.jade.wireless_optical_hatch.receptors_entry",
                     index, coords).withStyle(ChatFormatting.WHITE));
             index++;
         }
     }
 
-    private List<BlockPos> readReceivers(ListTag list) {
+    private List<BlockPos> readReceptors(ListTag list) {
         List<BlockPos> result = new ArrayList<>();
         for (int i = 0; i < list.size(); i++) {
             int[] coords = ((IntArrayTag) list.get(i)).getAsIntArray();
