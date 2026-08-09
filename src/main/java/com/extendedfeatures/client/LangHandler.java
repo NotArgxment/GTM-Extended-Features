33938f786fc91bb597dafd5fa4cec9923ab493b7
Usuario <therealxkorpion@gmail.com>
2026-08-09 17:58:51 -0300

    Added new lines and changed others
    Signed-off-by: Usuario <therealxkorpion@gmail.com>

--- src/main/java/com/extendedfeatures/client/LangHandler.java
+++ src/main/java/com/extendedfeatures/client/LangHandler.java
@@ -22,9 +22,9 @@ public class LangHandler {
         replace(provider, "block.extendedfeatures.rock_processing_plant", "Rock Processing Plant [RPP]");
         replace(provider, "block.extendedfeatures.industrial_greenhouse", "Industrial Greenhouse [IGh]");
         replace(provider, "block.extendedfeatures.tree_growing_chamber", "Tree Growing Chamber [TGCh]");
-        replace(provider, "block.extendedfeatures.zpm_data_access_hatch", "Elite Data Access Hatch");
-        replace(provider, "block.extendedfeatures.uv_data_access_hatch", "Ultimate Data Access Hatch");
-        replace(provider, "block.extendedfeatures.uhv_data_access_hatch", "Epic Data Access Hatch");
+        replace(provider, "block.extendedfeatures.zpm_data_access_hatch", "§cElite §rData Access Hatch");
+        replace(provider, "block.extendedfeatures.uv_data_access_hatch", "§3Ultimate §rData Access Hatch");
+        replace(provider, "block.extendedfeatures.uhv_data_access_hatch", "§4Epic §rData Access Hatch");
 
         // Recipe types lang
         replace(provider, "extendedfeatures.greenhouse_wood_recipes", "Tree Growing");
@@ -34,10 +34,10 @@ public class LangHandler {
         replace(provider, "extendedfeatures.chemical_skips", "Chemical Reduction");
 
         // Configuration lang
-        replace(provider, "config.screen.extendedfeatures", "§7Mod Configuration §4(Restart to Apply Changes)");
+        replace(provider, "config.screen.extendedfeatures", "§7Mod Configuration §c(Restart to Apply Changes)");
 
         replace(provider, "config.extendedfeatures.option.Multiblocks", "§7Multiblocks");
-        replace(provider, "config.extendedfeatures.option.RegularMachine", "§7Optical Features");
+        replace(provider, "config.extendedfeatures.option.RegularMachines", "§7Machines");
         replace(provider, "config.extendedfeatures.option.UniversalCircuits", "§7Universal Circuits");
 
         replace(provider, "config.extendedfeatures.option.RobustAlloyMaterializer", "§7Robust Alloy Materializer");
@@ -50,47 +50,48 @@ public class LangHandler {
         replace(provider, "config.extendedfeatures.option.TreeGrowingChamber", "§7Tree Growing Chamber");
         replace(provider, "config.extendedfeatures.option.Disassembler", "§7Disassembler");
         replace(provider, "config.extendedfeatures.option.LargeAirCollector", "§7Large Air Collector");
-
+        replace(provider, "config.extendedfeatures.option.MatrixDataRelay", "§7Matrix Data Relay");
         replace(provider, "config.extendedfeatures.option.ExpandedDatabank", "§7Expanded Data Bank");
+
         replace(provider, "config.extendedfeatures.option.ExpandedDataAccessHatches", "§7Expanded Data Access Hatches");
         replace(provider, "config.extendedfeatures.option.WirelessOpticalHatches", "§7Wireless Optical Tranmisssors/Receptors");
-        replace(provider, "config.extendedfeatures.option.CloudTransmissionDatabase", "§7Cloud Transmission Database ");
+        replace(provider, "config.extendedfeatures.option.CCMHatch", "§7Configurable Cleaning Maintenance Hatch");
 
         // Optical
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.range", "Scan range: §f%s blocks§7 (right-click with an empty hand)");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", "Max linked receptors: §f%s");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.scan", "Links to nearby unlinked receptors of the same tier, and to any physical Data Access Hatch in range");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.receptor", "Gets linked automatically when scanned by a Wireless Transmission Hatch of the same tier");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.not_formed", "This multiblock is not formed - form the structure before scanning");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.linked_summary", "Linked %s new receptor(s) and %s new data hatch(es)");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.no_receptors_found", "No new compatible receptors or data hatches found in range");
-        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.range_shown", "Displaying current range of connections: %s blocks in X/Z");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.range", "§fScan range:§f %s blocks §8(right-click with an empty hand)");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.connections", "§7Max links allowed:§f %s");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.scan", "§7Links to nearby Wireless Receptors and Data Access Hatches across its range");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.tooltip.receptor", "§7Gets linked automatically when scanned by a Wireless Transmissor of the same tier");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.not_formed", "§cThe hatch is not placed on a valid strucure!");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.linked_summary", "§fLinked§6 %s §fnew receptor(s) and§6 %s §fnew data hatch(es)");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.no_receptors_found", "§cNo new receptors or data hatches were found in range");
+        replace(provider, "extendedfeatures.machine.wireless_optical_hatch.range_shown", "§fDisplaying current range of connections:§c %s blocks");
 
         // Jade integration
         replace(provider, "config.jade.plugin_extendedfeatures.wireless_optical_hatch", "Wireless Optical Info");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_data_hatches", "Linked Data Access Hatches: %s");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_receptors_header", "Linked Wireless Optical Receptors:");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_data_hatches", "§fLinked Data Access Hatches: §6%s");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.linked_receptors_header", "§fLinked Wireless Optical Receptors:");
         replace(provider, "extendedfeatures.jade.wireless_optical_hatch.receptors_entry", "   - Receptor %s: %s");
-        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.no_receptors", "   - Not linked");
+        replace(provider, "extendedfeatures.jade.wireless_optical_hatch.no_receptors", "§c   - Not linked");
 
     }
 
     private static void Tooltips(RegistrateLangProvider provider) {
 
-        provider.add("extendedfeatures.compact_assembly_line.tooltip.0", "§7This machine performs the work of the Assembly Line without §9Ordered Inputs");
+        provider.add("extendedfeatures.compact_assembly_line.tooltip.0", "§fThis machine performs the work of the Assembly Line without §9Ordered Inputs");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.1", "§fAllows §bone §fenergy hatch");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.2", "§fRuns §b4 §frecipes in parallel");
         provider.add("extendedfeatures.compact_assembly_line.tooltip.3", "§fOnly works using");
 
         provider.add("extendedfeatures.synthesis_vessel.tooltip.0", "§fEven larger Black Box Reactor");
-        provider.add("extendedfeatures.synthesis_vessel.tooltip.1", "§7This machine is an improved version of the Large Chemical reactor capable of performing whole chemical lines in just 1 step");
+        provider.add("extendedfeatures.synthesis_vessel.tooltip.1", "§fThis machine is an improved version of the Large Chemical reactor capable of performing whole chemical lines in just 1 step");
 
-        provider.add("extendedfeatures.rock_processing_plant.tooltip.0", "§7An all in one processing line, turns the rocks you normally get from the rock breaker into their direct processed outputs");
+        provider.add("extendedfeatures.rock_processing_plant.tooltip.0", "§fAn all in one processing line, turns the rocks you normally get from the rock breaker into their direct processed outputs");
 
-        provider.add("extendedfeatures.greenhouse.tooltip.0", "§7Designed to make wood generation easier");
+        provider.add("extendedfeatures.greenhouse.tooltip.0", "§fDesigned to make wood generation easier");
         provider.add("extendedfeatures.greenhouse.tooltip.1", "§fRuns §b8 Recipes §fin parallel");
 
-        provider.add("extendedfeatures.disassembler.tooltip.0", "§7Allows any type of §emachine/controller §7to be recycled, returning what was used for that recipe");
+        provider.add("extendedfeatures.disassembler.tooltip.0", "§fAllows any type of §emachine/controller §fto be recycled, returning what was used for that recipe");
         provider.add("extendedfeatures.disassembler.tooltip.1", "§fEach tier of machine needs the same energy hatch of that tier in order to work");
 
         provider.add("extendedfeatures.expanded_databank_tootip.1", "§fAllows 16 independent Optical Transmission Hatches");
@@ -98,8 +99,9 @@ public class LangHandler {
         provider.add("extendedfeatures.matrix_data_relay.tooltip.1", "§fYour personal Data Center");
         provider.add("extendedfeatures.matrix_data_relay.tooltip.2", "§fOnly allows"); // followed by "extendedfeatures.styled.tooltip.4"
 
-        provider.add("extendedfeatures.large_air_collector.tooltip.0", "§7A Bigger Gas Collector");
-        provider.add("extendedfeatures.air_processing_machine.tooltip.0", "§7A machine capable of processing the air from the environment to get valuable products from it");
+        provider.add("extendedfeatures.large_air_collector.tooltip.0", "§fA Bigger Gas Collector");
+
+        provider.add("extendedfeatures.configurable_cleaning_maintenance_hatch", "§fFor configurable multiblocks maintenance with Cleaning!");
 
         provider.add("extendedfeatures.regular.tooltip.1", "§fAllows");
         provider.add("extendedfeatures.regular.tooltip.2", "§fand");
